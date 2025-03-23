959f609771bc42f0a7ac096deb345e52809d3aaf
hiteshkoppula <reddyhitesh60@gmail.com>
2025-03-23 13:28:54 -0500

    init commit

---
+++ src/test/java/com/sbtraining/Springboot/tutorial/SpringBootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbtraining.Springboot.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
